e5d75e21683bef0bf2c5a98f573bf1f56752b164
miteshparadox <ravimandal.here@gmail.com>
2026-09-02 23:16:21 +0545

    Fix Vercel server module import

--- api/index.ts
+++ api/index.ts
@@ -1,4 +1,4 @@
-import app from '../server';
+import app from '../server.ts';
 
 export default function handler(req: any, res: any) {
   const apiPath = typeof req.query?.path === 'string' ? req.query.path : '';
